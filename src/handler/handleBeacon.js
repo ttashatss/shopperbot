e864c9fda620394d1b78cf88fce8ad8e783a808f
ttashatss <tasha.tanarugs@gmail.com>
2024-03-31 18:28:43 +0700

    Beacon module fix

--- src/handler/handleBeacon.js
+++ src/handler/handleBeacon.js
@@ -1,70 +1,41 @@
 //  Establish connection and share information with Beacons
 
 import pushText from "../utils/pushText.js";
+import formatDate from "../utils/formatDate.js";
 
-var user_info = []
-var lastResetDate = new Date().getDate(); // Initialize with the current date
+var userData = [];
+var lastResetDate = new Date(); // Initialize with the current date
 
-// reset user_info everyday
-function resetListIfNeeded() {
-  let currentDate = new Date().getDate();
-  if (currentDate !== lastResetDate) {
-      // Reset the list to be empty
-      user_info = [];
-      // Update the last reset date
-      lastResetDate = currentDate;
-  }
-}
-// Call resetListIfNeeded function periodically, for example every hour
-setInterval(resetListIfNeeded, 3600000); // Call every hour (3600000 milliseconds)
+// for testing
+// const hwid = 123456;
+// const text = "wassupgurl";
 
 const handleBeacon = (client, userId, event) => {
-  const type = event.beacon.type;
+  // Beacon specific variables
   const hwid = event.beacon.hwid;
-  const timestamp = event.timestamp;
-  var date = new Date(timestamp);
-  var info = [userId, hwid]
-  var updated = false;
   const dm = `${Buffer.from(event.beacon.dm || "", "hex").toString("utf8")}`;
   const text = `${dm}`;
 
-  // Format datetime
-  let year = date.getFullYear();
-  let month = ('0' + (date.getMonth() + 1)).slice(-2); // Add 1 to month because months are zero-based
-  let day = ('0' + date.getDate()).slice(-2);
-  let hours = ('0' + date.getHours()).slice(-2);
-  let minutes = ('0' + date.getMinutes()).slice(-2);
-  let seconds = ('0' + date.getSeconds()).slice(-2);
-  let datetimeString = year + '-' + month + '-' + day + ' ' + hours + ':' + minutes + ':' + seconds;
+  const timestamp = event.timestamp;
+  const date = formatDate(timestamp)[0];
 
-  // update user_info if found in the list. If found, update and do nothing.
-  if (user_info.length != 0) {
-    for (let i = 0; i < user_info.length; i++) {
-        if (user_info[i][0] == info[0]) {
-          user_info[i][1] == info[1];
-          updated = true;
-          console.log(user_info)
-          console.log(datetimeString)
-          return;
-        }
-    }
-    // if not found, append new data to user_info
-    if (!updated) {
-        user_info.push(info);
-        console.log(user_info)
-        console.log(datetimeString)
-        return pushText(client, userId, text);
-    }
+  // Reset if current date is different from lastResetDate
+  if (date !== formatDate(lastResetDate)[0]) {
+    userData = [];
+    lastResetDate = new Date();
+    console.log("userData is reset");
   }
 
-  user_info.push(info)
-  // console.log(event.beacon)
-  // console.log(datetimeString)
-  console.log(user_info)
-  console.log(datetimeString)
-  // console.log(date)
-  console.log(lastResetDate)
-  return pushText(client, userId, text);
+  const idIndex = userData.findIndex((element) => element[0] == userId);
+  if (idIndex == -1) {
+    userData.push([userId, [hwid]]);
+    console.log(userData);
+    return pushText(client, userId, text);
+  } else {
+    userData[idIndex][1].unshift(hwid);
+    console.log(userData);
+    return;
+  }
 };
 
 export default handleBeacon;
---
+++ src/utils/resetList.js
@@ -0,0 +1,11 @@
+// // Reset list daily
+
+// const resetList = (list, previousDate) => {
+//     let currentDate = new Date().getDate();
+//     if (currentDate !== previousDate) {
+//         list = []; 
+//         return [list, currentDate]
+//     }
+// }
+
+// export default resetList;
